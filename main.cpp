8beef82b98726de9ef9aa98a8b2a25180f2a07a6
Brandon Ekbatani <bekbatani@mail.csuchico.edu>
2019-02-20 11:36:12 -0800

    Fixed Decending order function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
+//Brandon Ekbatani 
+
 #include <iostream>
 using std::cout;
 using std::cin;
 using std::endl;
 
-void sortDescending(int,int,int);
+void sortDescending(int,int, int);
 void swap(int&,int&);
 
 int main()
@@ -13,31 +15,34 @@ int main()
   cin>>numA>>numB>>numC;
 
   sortDescending(numA, numB, numC);
-  
-  cout<<"From greatest to least, they are: ";
-  cout<<numA<<","<<numB<<","<<numC<<endl;
   return 0;
 }
 
 void sortDescending(int first, int second, int third)
 {
-  if( first < third )
+
+  if(first < third)
   {
-    swap(first,third);
+    swap(first, third);
   }
-  if( first < second )
+   if(first < second)
   {
-    swap(first,second);
+    swap(first, second);
   }
-  if( second < third )
+   if(second < third)
   {
-    swap(second,third);
+    swap(second, third);
   }
+     cout<<"From greatest to least, they are: ";
+
+  cout<<first<<","<<second<<","<<third<<endl;
+
 }
 
-void swap(int &first, int &second)
+void swap(int &first, int &second) 
 {
   int temp = first;
   first = second;
   second = temp;
+
 }
